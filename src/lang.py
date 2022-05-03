ea8d336e8e7d1f91b766e7dfb147098a4bc9329b
sevenc-nanashi <sevenc-nanashi@sevenbot.jp>
2022-05-03 22:34:34 +0900

    Code: Command.qualified_nameで簡略化

--- src/lang.py
+++ src/lang.py
@@ -16,15 +16,12 @@ def text(self: discord.Interaction, code: str, **formats) -> str:
         with open(get_locale(self.locale), "r", encoding="utf-8") as file:
             texts = yaml.safe_load(file)
         command = self.command
-        if isinstance(self.command, discord.app_commands.Command):
-            if not command.parent:
-                base = texts["chat_input"][command.name]
-            elif command.parent == command.root_parent:  # Subcommand
-                base = texts["chat_input"][command.parent.name][command.name]
-            else:  # Subcommand group
-                base = texts["chat_input"][command.root_parent.name][command.parent.name][command.name]
+        if isinstance(command, discord.app_commands.Command):
+            base = texts["chat_input"]
+            for name in command.qualified_name.split(" "):
+                base = base[name]
         else:  # Context
-            base = texts["context"][str(self.command)]
+            base = texts["context"][str(command)]
         for code_key in code.split("."):
             base = base[code_key]
         return base.format(**formats)
