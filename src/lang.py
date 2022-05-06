ff1184b501f688b886c908c0eeb8b57d4ee20616
sevenc-nanashi <sevenc7c@sevenc7c.com>
2022-05-06 23:29:01 +0900

    Change: guild_textでもqualified_nameを使うように

--- src/lang.py
+++ src/lang.py
@@ -34,13 +34,10 @@ def guild_text(self: discord.Interaction, code: str, **formats) -> str:
         with open(get_locale(self.guild_locale), "r", encoding="utf-8") as file:
             texts = yaml.safe_load(file)
         command = self.command
-        if command.type == discord.AppCommandType.chat_input:
-            if not command.parent:
-                base = texts["chat_input"][command.name]
-            elif command.parent == command.root_parent:  # Subcommand
-                base = texts["chat_input"][command.parent.name][command.name]
-            else:  # Subcommand group
-                base = texts["chat_input"][command.root_parent.name][command.parent.name][command.name]
+        if isinstance(command, discord.app_commands.Command):
+            base = texts["chat_input"]
+            for name in command.qualified_name.split(" "):
+                base = base[name]
         else:
             base = texts["context"][str(self.command)]
         for code_key in code.split("."):
